9f63dee9fc29a16d251e3da0c40022aca1987914
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-12 22:43:11 -0300

    :ambulance: add globals vars

---
+++ R/globals.R
@@ -0,0 +1,8 @@
+#' @import utils
+utils::globalVariables(c("created_at", "game_id", "glue", "id", "is_author",
+                         "is_expert", "is_verified", "league_name", "name",
+                         "num_followers.total", "play", "stat", "type",
+                         "updated_at", "user_id", "value"))
+
+
+
