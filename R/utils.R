f72ecca7172593007ed4c0db133d1b24f3a73967
Daniel Amaral <silva.daniel86@Gmail.com>
2021-12-13 11:14:58 -0300

    :sparkles: add support to column types in empty_tibble function

--- R/utils.R
+++ R/utils.R
@@ -4,17 +4,20 @@
 #' as parameter.
 #'
 #' @param column_names character vector with column names
+#' @param column_types character vector with column types
 #'
 #' @importFrom tibble tibble
 #' @importFrom purrr map
+#' @importFrom glue glue
+#' @importFrom rlang enexpr parse_expr
 #'
 #' @return empty tibble object
 #' @export
 #'
 #' @examples
-#' empty_tibble(c("a", "b"))
-empty_tibble <- function(column_names) {
-  args <- map(column_names, ~ character())
+#' empty_tibble(c("a", "b"), c("character", "integer"))
+empty_tibble <- function(column_names, column_types) {
+  args <- map(column_types, \(ct) eval(parse_expr(glue('{enexpr(ct)}()'))))
   names(args) <- column_names
   do.call(tibble, args)
 }
